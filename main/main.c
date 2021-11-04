3fa1b0c10b40b72b4e3b1dec02d6c1385db851bf
chengxiaoyu <writeforever@foxmail.com>
2021-11-04 09:40:37 +0800

    daily update.

--- main/main.c
+++ main/main.c
@@ -22,6 +22,8 @@
 #include "driver/gpio.h"
 #include "driver/dht11.h"
 
+#if 0 
+//Register of Timer group 1 Timer0
 #define TIMG1_T0CONFIG_REG (*(volatile unsigned int *)0x60020000)
 #define TIMG1_T0LO_REG (*(volatile unsigned int *)(0x60020000 + 0x0004))
 #define TIMG1_T0HI_REG (*(volatile unsigned int *)(0x60020000 + 0x0008))
@@ -29,6 +31,7 @@
 #define TIMG1_T0LOADLO_REG (*(volatile unsigned int *)(0x60020000 + 0x0018))
 #define TIMG1_T0LOADHI_REG (*(volatile unsigned int *)(0x60020000 + 0x001c))
 #define TIMG1_T0LOAD_REG (*(volatile unsigned int *)(0x60020000 + 0x0020))
+#endif 
 
 static psDHT11_operations dht11_opr;
 static     dht11_handle_t dht11;
@@ -66,14 +69,6 @@ void dht11_cb(xTimerHandle xTimer)
 {
     // int ret;
 
-
-    // uint8_t hum_h = dht11_opr->read_byte();
-    // uint8_t hum_l = dht11_opr->read_byte();
-    // uint8_t tem_h = dht11_opr->read_byte();
-    // uint8_t tem_l = dht11_opr->read_byte();
-    // uint8_t sum = dht11_opr->read_byte();
-    // printf("温度: %0.2f℃\t湿度: %0.2f%%\n",
-    //        tem_h + tem_l / 100.0, hum_h + hum_l / 100.0);
     dht11_opr->decode(&dht11);
     printf("[ %0.2f ] 温度: %0.2f℃\t湿度: %0.2f%%\n",
            //    dht11.last_decode_time/1000,
